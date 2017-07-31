138142f5979276fdf93fb122949b42887fd7fae4
Mateus Koppe <mateus.koppe@gmail.com>
2017-07-30 23:36:02 -0300

    Filter refactoring

--- generators/filter/index.js
+++ generators/filter/index.js
@@ -1,50 +1,56 @@
-let Generator = require('yeoman-generator');
+'use strict';
 
-module.exports = class extends Generator {
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator{
+
   constructor(args, opts) {
     super(args, opts);
 
-    if (!this._isArgsValids(args)) {
-      this.log.error('Sintax error, you must use this sintax: batangularjs:filter [module] [filter] <-tc>');
-      return;
-    }
+    this.args = args;
+    this.opts = opts;
 
-    this.moduleName = args[0];
-    this.filterName = args[1];
-
-    if (this.moduleName == 'app') {
-      this.moduleFolder = 'app/';
-      this.moduleName = 'app';
-    } else {
-      this.moduleFolder = `app/${this.moduleName}/`;
-      this.moduleName = `app.${this.moduleName}`;
-    }
-
-    this.preFolder = '';
-    if (opts.c) {
-      this.preFolder += 'core/';
-    }
-    if (opts.t) {
-      this.preFolder += 'filters/';
-    }
-
-    this._writeProject();
   }
 
-  method() {}
-
-  _isArgsValids(args) {
-    return args.length >= 2;
+  validateArgs(args){
+    if(!this.args.length > 2){
+      this.env.error('Sintax error, you must use the sintax: batangularjs:filter [filter] <-tc>');
+    }
   }
 
-  _writeProject() {
+  args(){
+    this.moduleName = this.args[0];
+    this.filterName = this.args[1];
+  }
+
+  folder(){
+    this.dest = 'app/';
+    if(this.moduleName !== 'app') {
+      this.dest +=  `${this.moduleName}/`;
+    }
+    if(this.opts.c){
+      this.dest +=  `core/`;
+    }
+    if(this.opts.t){
+      this.dest +=  `filters/`;
+    }
+  }
+
+  file(){
+    this.file = `${this.filterName}.filter.js`;
+  }
+
+  writing(){
     this.fs.copyTpl(
       this.templatePath('filter.js'),
-      this.destinationPath(`${this.moduleFolder}${this.preFolder}${this.filterName}.filter.js`),
+      this.destinationPath(`${this.dest}${this.file}`),
       {
         moduleName: this.moduleName,
         filterName: this.filterName
       }
     );
   }
-};
+
+}
