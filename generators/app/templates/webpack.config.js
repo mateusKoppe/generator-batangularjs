1e4379ee8484415a9f058c1dbc849ff301f5aeb7
Mateus Koppe <mateus.koppe@gmail.com>
2017-10-30 00:19:27 -0200

    Config webpack

---
+++ generators/app/templates/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+    filename: 'bundle.css'
+})
+
+module.exports = {
+    entry: './app/app.module.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin
+    ]
+};
