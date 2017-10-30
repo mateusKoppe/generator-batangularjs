1f41cd53c792002f2f50e71e31d92b786afb2a0e
Mateus Koppe <mateus.koppe@gmail.com>
2017-10-30 02:55:11 -0200

    Change app test

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -11,8 +11,15 @@ describe('generator-batangularjs:app', () => {
   let files = [
     'index.html',
     'package.json',
+    'webpack.config.js',
+    'app/app.component.js',
     'app/app.module.js',
-    'app/app.style.scss'
+    'app/app.scss',
+    'app/components/components.module.js',
+    'app/components/home/home.module.js',
+    'app/components/home/home.component.js',
+    'app/components/home/home.html',
+    'app/common/common.module.js'
   ];
 
   files.forEach(file => it(`create ${file}`, () => assert.file([file])));
