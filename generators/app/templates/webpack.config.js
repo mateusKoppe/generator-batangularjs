fae936536540a05d5e2d33af551e4fa2176ae840
Mateus Koppe <mateus.koppe@gmail.com>
2017-10-31 17:10:47 -0200

    Add ngannotate loader

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -2,45 +2,46 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractPlugin = new ExtractTextPlugin({
-    filename: 'bundle.css'
+  filename: 'bundle.css'
 })
 
 module.exports = {
-    entry: './app/app.module.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: extractPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    { loader: 'ngtemplate-loader?relativeTo=' + __dirname + '/' },
-                    { loader: 'html-loader' }
-                ]
-            },
+  entry: './app/app.module.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          { loader: 'ng-annotate-loader' },
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          },
         ]
-    },
-    plugins: [
-        extractPlugin,
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: [
+          { loader: 'ngtemplate-loader?relativeTo=' + __dirname + '/' },
+          { loader: 'html-loader' }
+        ]
+      },
     ]
+  },
+  plugins: [
+    extractPlugin,
+  ]
 };
