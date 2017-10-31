a29431bae8b07a41e1ef1e914bf43bb45ab4dd91
Mateus Koppe <mateus.koppe@gmail.com>
2017-10-31 12:56:07 -0200

    Directive adjust

--- generators/directive/templates/directive.js
+++ generators/directive/templates/directive.js
@@ -3,14 +3,8 @@ export const <%= directiveName %>Directive = () => {
     restrict: 'A',
     scope: {},
     transclude: false,
-    template: `
-      <div></div>
-    `,
     link: (scope, element, attrs) => {
 
-    },
-    controller: class <%= directiveName %>Controller {
-      constructor(){}
     }
   }
 };
