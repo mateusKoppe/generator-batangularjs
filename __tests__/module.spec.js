21de868607793940f9310922260cdd6bf2e4ab21
Mateus Koppe <mateus.koppe@gmail.com>
2017-11-03 02:58:42 -0200

    test(module): Add component by param
    -c -> will generate a component
    -t -> will generate a component template

--- __tests__/module.spec.js
+++ __tests__/module.spec.js
@@ -55,3 +55,68 @@ describe('Testing module with route', () => {
   );
 });
 
+describe('Testing module with component', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/module'))
+      .withArguments(['userAuth'])
+      .withOptions({c: true});
+  });
+
+  const fileDir = 'app/user-auth/user-auth.component.js';
+
+  it('creates files', () => {
+    assert.file([fileDir]);
+  });
+
+  it('define name', () =>
+    assert.fileContent(
+      fileDir,
+      'export const UserAuthComponent = {'
+    )
+  );
+
+  it('define template', () =>
+    assert.fileContent(
+      fileDir,
+      'template: '
+    )
+  );
+
+  it('not define templateUrl', () =>
+    assert.noFileContent(
+      fileDir,
+      'templateUrl'
+    )
+  );
+});
+
+describe('Testing module with components and template', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/module'))
+      .withArguments(['userAuth'])
+      .withOptions({c: true, t: true});
+  });
+
+  const fileDir = 'app/user-auth/user-auth.component.js';
+
+  it('creates files', () => {
+    assert.file([
+      fileDir,
+      fileDir.replace('component.js', 'component.html') // Html file
+    ]);
+  });
+
+  it('not define template', () =>
+    assert.noFileContent(
+      fileDir,
+      'template: '
+    )
+  );
+
+  it('define templateUrl', () =>
+    assert.fileContent(
+      fileDir,
+      'templateUrl'
+    )
+  );
+});
