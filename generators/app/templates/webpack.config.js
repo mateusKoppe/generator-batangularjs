3bb37f54d9361a4f27ec102128477398088684b6
Everton Dalla Corte <everton@o2multi.com.br>
2017-11-09 15:25:38 -0200

    option removed of url-loader, removed file-loader

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|svg)$/,
         use:[
-          { loader: 'url-loader?limit=100000' }
+          { loader: 'url-loader' }
         ]
       }
     ]
