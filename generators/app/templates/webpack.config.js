dc9ca7f8d2ce293e6508341e4bc3f1bb362910cd
Mateus Koppe <mateus.koppe@gmail.com>
2017-11-10 05:16:29 -0200

    chore(template): Remove url-loader

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -50,51 +50,9 @@ module.exports = {
         ]
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/,
+        test: /\.(jpe?g|png|gif|svg|woff?2|ttf|eot)$/,
         use: [ 'file-loader' ]
       },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "url-loader",
-            options:{
-              limit: 10000,
-              mimetype: "application/font-woff"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "url-loader",
-            options:{
-              limit: 10000,
-              mimetype: "application/font-woff"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "url-loader",
-            options:{
-              limit: 10000,
-              mimetype: "application/octet-stream"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          { loader: "file-loader" }
-        ]
-      }
     ]
   },
   plugins: [
